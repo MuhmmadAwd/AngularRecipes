668473ebb4c57b57468ff7e9366921a26565ea0a
mohamad awad <noza25588@gmail.com>
2022-05-19 14:23:23 +0300

    clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RecipeProject';
-  route:string;
-  onNavigate(Route:string){
-    this.route=Route
-  }
+
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,4 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/Ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
@@ -8,7 +8,6 @@ import { Recipe } from './recipe.model';
 })
 export class RecipeService {
   constructor(public _shoppingListService: ShoppingListService) {}
-  recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe(
       'nameTest',
